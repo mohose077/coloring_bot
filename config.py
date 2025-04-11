4891576d3a58abfe4d67450739fa110b85acdaf8
mohose077 <mail.work071@gmail.com>
2025-04-11 15:00:45 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,8 @@
+# config.py
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 REPLICATE_API_TOKEN = os.getenv("REPLICATE_API_TOKEN")
